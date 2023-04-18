e92cec9947d4a4f3646f2fdac24964dc21f8f98f
Wout Ongena <woutongena@gmail.com>
2023-04-19 01:29:37 +0200

    Opdracht 2: added progressbar

--- opdrachten/Once Upon AI/js/main.js
+++ opdrachten/Once Upon AI/js/main.js
@@ -1,5 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
+let main = document.querySelector("main");
 const sections = document.querySelectorAll("section");
 
 sections.forEach((section) => {
@@ -10,10 +11,20 @@ sections.forEach((section) => {
     duration: 1,
     opacity: 0,
     scrollTrigger: {
-      scroller: "main",
+      scroller: main,
       trigger: section,
       start: "top center",
       toggleActions:"play reverse play reverse"
     },
-  });
+    ease: "back.out(0.7)"
+    });
+});
+
+main.addEventListener('scroll', function() {
+  const vhInPx = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+  var scrollPosition = main.scrollY;
+  var progressBar = document.querySelector('.loadingBarProgress');
+  var scrollPosition = (main.scrollTop + vhInPx) / main.scrollHeight;
+  
+  progressBar.style.height = scrollPosition * 100 + '%';
 });
