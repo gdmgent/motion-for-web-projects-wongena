95c18a508b84d9a3c4a3294027976e1138365144
Wout Ongena <woutongena@gmail.com>
2023-05-05 23:53:44 +0200

    Added advanced version

--- testFolder/warpdrive/js/script.js
+++ testFolder/warpdrive/js/script.js
@@ -1,64 +1,90 @@
-//tutorial: https://www.youtube.com/watch?v=Bed1z7f1EI4
+//tutorial: https://www.youtube.com/watch?v=MfOixPz65ag
 
 import * as THREE from "https://cdn.skypack.dev/pin/three@v0.132.2-1edwuDlviJO0abBvWgKd/mode=imports/optimized/three.js";
-let scene, camera, renderer, starBox, stars, vertices;
+var scene, camera, renderer;
+
+let LINE_COUNT = 1000;
+let geom = new THREE.BufferGeometry();
+geom.setAttribute(
+  "position",
+  new THREE.BufferAttribute(new Float32Array(6 * LINE_COUNT), 3)
+);
+geom.setAttribute(
+  "velocity",
+  new THREE.BufferAttribute(new Float32Array(2 * LINE_COUNT), 1)
+);
+let pos = geom.getAttribute("position");
+let pa = pos.array;
+let vel = geom.getAttribute("velocity");
+let va = vel.array;
 
 function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.z = 1;
-    camera.rotation.x = Math.PI / 2;
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+  camera.position.z = 200;
 
-    renderer = new THREE.WebGLRenderer;
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-    starBox = new THREE.BufferGeometry();
-    vertices = {
-        positions: [],
-        accelerations: [],
-        velocities: [],
-    };
-    for(let i = 0; i < 18000; i++) {
-        vertices.positions.push(Math.random() * 600 - 300);
-        if(i % 3 === 0) {
-            vertices.accelerations.push(0);
-            vertices.velocities.push(.2);
-        }
-    }
-    starBox.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices.positions), 3));
+  for (let line_index = 0; line_index < LINE_COUNT; line_index++) {
+    var x = Math.random() * 400 - 200;
+    var y = Math.random() * 200 - 100;
+    var z = Math.random() * 500 - 100;
+    var xx = x;
+    var yy = y;
+    var zz = z;
+    //line start
+    pa[6 * line_index] = x;
+    pa[6 * line_index + 1] = y;
+    pa[6 * line_index + 2] = z;
+    //line end
+    pa[6 * line_index + 3] = xx;
+    pa[6 * line_index + 4] = yy;
+    pa[6 * line_index + 5] = zz;
 
-    let starImage = new THREE.TextureLoader().load('./images/star.png');
-    let starMaterial = new THREE.PointsMaterial({
-        color: 0xaaaaaa,
-        size: 0.7,
-        map: starImage
-    });
-
-    stars = new THREE.Points(starBox, starMaterial);
-    scene.add(stars);
-
-    animate();
-}
-
-function animate() {
-    for(let i = 0; i < vertices.velocities.length; i++) {
-        vertices.velocities[i / 3 + i % 3] += vertices.accelerations[i];
-        vertices.positions[i * 3 + 1] -= vertices.velocities[i];
-        if(vertices.positions[i*3 +1] < -200) {
-            vertices.positions[i * 3 + 1] = 400;
-            vertices.velocities[i / 3 + i % 3] = 0;
-        }
-    }
-    stars.rotation.y += 0.001;
-    starBox.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices.positions), 3));
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
-}
-
-init();
-
-function onWindowResize () {
-    renderer.setSize (window.innerWidth, window.innerHeight);
-    renderer.setViewport (0, 0, window.innerWidth, window.innerHeight);
+    va[2 * line_index] = va[2 * line_index + 1] = 0;
   }
+  //debugger;
+  let mat = new THREE.LineBasicMaterial({ color: 0xffffff });
+  let lines = new THREE.LineSegments(geom, mat);
+  scene.add(lines);
+
+  window.addEventListener("resize", onWindowResize, false);
+  animate();
+}
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+function animate() {
+  for (let line_index = 0; line_index < LINE_COUNT; line_index++) {
+    va[2 * line_index] += 0.01; //bump up the velocity by the acceleration amount
+    va[2 * line_index + 1] += 0.025;
+
+    //pa[6*line_index]++;                       //x Start
+    //pa[6*line_index+1]++;                     //y
+    pa[6 * line_index + 2] += va[2 * line_index]; //z
+    //pa[6*line_index+3]++;                     //x End
+    //pa[6*line_index+4]                        //y
+    pa[6 * line_index + 5] += va[2 * line_index + 1]; //z
+
+    if (pa[6 * line_index + 5] > 200) {
+      var z = Math.random() * 200 - 100;
+      pa[6 * line_index + 2] = z;
+      pa[6 * line_index + 5] = z;
+      va[2 * line_index] = 0;
+      va[2 * line_index + 1] = 0;
+    }
+  }
+  pos.needsUpdate = true;
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+init();
